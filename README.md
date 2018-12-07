670e12d3171a8fabe3245ba780df43190250d8ae
Mark Jessop <vk5qi@rfhead.net>
2018-12-07 23:27:54 +1030

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# satnogs-rotator-homing
-Move a SatNOGS station's rotator to either the start of the next observation, or to a home location.
+# SatNOGS Rotator Un-Winder Utility
+
+This utility can be used to move a SatNOGS rotator station to an 'absolute' position between passes. This position could either be a 'home' location, or the start of the next pass. This helps 'unwind' a rotator prior to a pass occuring.
+
+## Motivation
+I operate SatNOGS station [#232](https://network.satnogs.org/stations/232/). It uses a RF HamDesign SPX-02 rotator, with a SPID Rot2Prog controller, and my setup is configured to provide 640 degrees of travel (-180 through to +460 degrees, where <0 degrees and > 360 degrees is an 'overwind' region).
+
+While hamlib's rot2prog driver will quite happily report the 'absolute' position of the rotator (i.e. if it's in an 'overwind' region it will report <0 or >360 degrees), you cannot *set* an absolute position. Instead, hamlib will direct the rotator to take the shortest path to the provided position.
+
+Unfortunately, this behaviour can lead to the rotator ending up in a 'wound-up' state where on the next pass, the movement of the rotator ends up moving into the overwind region and hitting a limit, and the rotator has to do a 360 degree spin to 'unwind' continue to track the satellite. On my rotator, it can take almost 2 minutes to perform this action, and results in [big signal gaps](https://network.satnogs.org/observations/316507/). Wouldn't it be good if we do this long 'unwind' movement *before* the pass?
+
+## Solution
+
+This problem has been discussed in a satnogs-client [issue](https://gitlab.com/librespacefoundation/satnogs/satnogs-client/issues/275), and I've suggested a fix in the LibreSpace [forum](https://community.libre.space/t/rotator-control-parking/2511/2?u=vk5qi). As a means of testing out a possible solution, I've created the `unwind.py` script in this directory. It's not an ideal solution, but it's a starting point.
+
+In short, since we can read the absolute rotator position from the Rot2Prog, we can move to a desired absolute position by moving in a sequence of small steps. I'm using steps of 90 degrees. Thic can be used to move back to a 'home' location, or even better - move to the start of the next observation!
+
+SatNOGS provides the handy `SATNOGS_POST_OBSERVATION_SCRIPT` option, which can be used to run a shell script after an observation has finished (duh). We run a script there which will move the rotator to the start of the next observation ahead of time, or alternatively move to a home location if there are no observations in the near (1 day) future.
+
+## Usage
+
+The following dependencies are required (and probably already available on a SatNOGS station):
+ * python-requests
+ * python-dateutil
+
+The `unwind.py` utility can be run in a few ways.
+
+To move to home location, run it with:
+```
+$ python unwind.py --home_azimuth=0.0 --home_elevation=0.0
+```
+
+By default we assume the movement will take less than 180 seconds. If your rotator takes longer than this to move (?!), then use the `--movement_timeout` parameter to set a different timeout.
+
+
+To move to the starting point of the next observation for your SatNOGS station, add on the --station_id=X argument, i.e.:
+```
+$ python unwind.py --home_azimuth=0.0 --home_elevation=0.0 --station_id=232
+```
+If there is no upcoming observation, the rotator will be moved to the home location. If the observation is closer in time than the movement timeout, then we will bomb out and not attempt to move. If your station is on network-dev, then add `--network_dev`.
+
+By default, we assume that there is a rotctld instance running on localhost:4533. If it's running elsewhere, then the `--rotctld_host` and `--rotctld_port` options can change this. 
+
+To run this after an observation, create a shell script in your home directory (or elsewhere) containing the above command, set it to executable (`chmod +x yourscript.sh`) and use `satnogs-setup` to define the post-observation script. Note that you might need to use an absolute path to the `unwind.py` utility. As an example, the script could be as simple as:
+```
+#!/usr/bin/env bash
+
+python /home/pi/satnogs-unwinder/unwind.py --home_azimuth=0.0 --home_elevation=0.0 --station_id=232
+
+```
+
+Run `python unwind.py --help` to see information on all the command-line options.
+
+## Warnings
+This script is a fairly quick hack, and will bomb out if there are any errors.
+
+It will *only* work if the rotator (via rotctld) reports *absolute* postions. Check this by manually moving your rotator into an overwind region, and request a position report by running:
+```
+$ nc localhost 4533
+p                                         <i.e. you type in `p` and press enter here, and you will see:>
+-90.000000                                <Azimuth>
+0.000000                                  <Elevation>
+```
+If you can get a value below 0 degrees, or above 360 degrees, then this script should work. Otherwise it may not.
+
+You may also encounter very bad behaviour if there is only a very short time between passes and your rotator takes too long to move. The `movement_timeout` argument, and the checking for close observations does help with this, but there may still be edge cases. Having two programs attempting to control one rotator is bad and may cause you to have a bad observation, or a bad day. 
+
+As per the GPL:
+```
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+```
+
---
+++ unwind.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+#
+#   SatNOGS Rotator Homing
+#
+#   Move a SPID Rotator (or any other rotator that will report *absolute* positions, i.e. -180 through 540 degrees)
+#   to an *absolute* position, in small steps.
+#
+#   The position an be set as either a 'home' position, but you can also provide a SatNOGS station ID,
+#   in which case the rotator will be moved to the start position of the next observation.
+#
+#   Copyright (C) 2018  Mark Jessop <vk5qi@rfhead.net>
+#   Released under GNU GPL v3 or later
+#
+import argparse
+import datetime
+import logging
+import time
+import requests
+import socket
+import sys
+
+from dateutil.parser import parse
+from dateutil.tz import *
+
+logging.basicConfig(level=logging.DEBUG)
+
+class ROTCTLD(object):
+    """ rotctld (hamlib) communication class """
+    # Note: This is a massive hack. 
+
+    def __init__(self, 
+        hostname="127.0.0.1", 
+        port=4533, 
+        timeout=5,
+        poll_rate=5,
+        movement_threshold=5.0,
+        movement_timeout=120.0):
+
+        """ Initiate the ROTCTLD Connection """
+        self.hostname = hostname
+        self.port = port
+        self.poll_rate = poll_rate
+        self.movement_threshold = movement_threshold
+        self.movement_timeout = movement_timeout
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(timeout)
+
+        self.connect()
+
+
+    def connect(self):
+        """ Connect to rotctld instance """
+        self.sock.connect((self.hostname,self.port))
+        model = self.get_model()
+        if model == None:
+            # Timeout!
+            self.close()
+            raise Exception("Timeout!")
+        else:
+            return model
+
+
+    def close(self):
+        self.sock.close()
+
+
+    def send_command(self, command):
+        """ Send a command to the connected rotctld instance,
+            and return the return value.
+        """
+        _command = command + '\n'
+        self.sock.sendall(_command.encode('ascii'))
+        try:
+            return self.sock.recv(1024).decode('ascii')
+        except:
+            return None
+
+
+    def get_model(self):
+        """ Get the rotator model from rotctld """
+        model = self.send_command('_')
+        return model
+
+
+    def set_azel(self,azimuth,elevation, blocking=False, timeout=120):
+        """ Command rotator to a particular azimuth/elevation """
+        # Sanity check inputs.
+        if elevation > 90.0:
+            elevation = 90.0
+        elif elevation < 0.0:
+            elevation = 0.0
+
+        if azimuth > 360.0:
+            azimuth = azimuth % 360.0
+
+
+        command = "P %3.1f %2.1f" % (azimuth,elevation)
+        response = self.send_command(command)
+
+        # If we don't get RPRT 0 in the response, this indicates
+        # an error commanding the rotator.
+        if "RPRT 0" not in response:
+            return False
+        else:
+            # If we *do* get RPRT 0, then we have successfully commanded the rotator.
+            if not blocking:
+                # If we're not in blocking more, return immediately.
+                return True
+
+        # Otherwise, we're going to wait for the rotator to reach its intended position.
+        _start_time = time.time()
+        logging.debug("Target position: %.1f, %.1f" % (azimuth, elevation))
+
+        # Keep checking the rotator position until we have hit our timeout.
+        while (time.time() - _start_time) < self.movement_timeout:
+            time.sleep(self.poll_rate)
+            (_az, _el) = self.get_azel()
+
+            # Immediately raise an exception if we can't get a position.
+            if _az is None:
+                raise Exception("No communication with rotator.")
+
+            logging.debug("Current position: %.1f, %.1f" % (_az, _el))
+
+            # Otherwise, compare with the target position.
+            if (abs(azimuth - _az%360.0) < self.movement_threshold) and (abs(elevation - _el) < self.movement_threshold) :
+                # We are there! (or close enough that we can break out of this loop)
+                return True
+            else:
+                continue
+
+        # We have hit the timeout.
+        raise Exception("Movement Timeout!")
+
+
+
+    def get_azel(self):
+        """ Poll rotctld for azimuth and elevation """
+        # Send poll command and read in response.
+        response = self.send_command('p')
+
+        # Attempt to split response by \n (az and el are on separate lines)
+        try:
+            response_split = response.split('\n')
+            _current_azimuth = float(response_split[0])
+            _current_elevation = float(response_split[1])
+            return (_current_azimuth, _current_elevation)
+        except:
+            logging.error("Could not parse position: %s" % response)
+            return (None,None)
+
+
+
+    def halt(self):
+        """ Immediately halt rotator movement, if it support it """
+        self.send_command('S')
+
+
+
+def get_next_rise_azimuth(station_id=1, dev=False):
+    ''' Query the SatNOGS network for upcoming scheduled observations for a station,
+        and return the rise azimuth of the next pass.
+
+        Args:
+            station_id (int): Station ID of the station
+            dev (bool): Use network-dev instead of network.
+
+        Returns:
+            float / None: None if there are no upcoming observations, otherwise the rise azimuth of the next observation.
+            float / None: None if there are no upcoming observations, otherwise, the number of seconds until the next observation.
+    '''
+
+    _dev = "-dev" if dev else ""
+    _request_url = "https://network%s.satnogs.org/api/observations/?ground_station=%d&vetted_status=unknown" % (_dev, station_id)
+
+    # https://network.satnogs.org/api/observations/?ground_station=232&vetted_status=unknown
+
+    try:
+        _r = requests.get(_request_url)
+        _obs = _r.json()
+    except Exception as e:
+        logging.error("Error getting next observation info - %s" % str(e))
+        return (None, None)
+
+    # The network API returns a list of observation objects.
+    if type(_obs) is not list:
+        logging.error("SatNOGS API did not return expected list.")
+        return (None, None)
+
+    # Check that there are actually some observations to look at.
+    if len(_obs) == 0:
+        logging.info("No scheduled observations found.")
+        return (None, None)
+    
+
+    # Observations are not always provided in time-sorted order, so we need to search for the earliest one.
+    # If we don't find one that's sooner than one day ahead, just go to the home position.
+    _earliest_obs_time = datetime.datetime.now(tzutc()) + datetime.timedelta(1)
+    _rise_az = None
+
+    for _o in _obs:
+        _start = parse(_o['start'])
+
+        if _start < _earliest_obs_time:
+            _earliest_obs_time = _start
+            _rise_az = _o['rise_azimuth']
+
+    _time_to_obs = (_earliest_obs_time - datetime.datetime.now(tzutc())).total_seconds()
+
+    return (_rise_az, _time_to_obs)
+
+
+
+if __name__ == "__main__":
+    # Read in command line arguments.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--station_id', type=int, default=-1, help="SatNOGS Station ID")
+    parser.add_argument('--network_dev', action='store_true', default=False, help="Use SatNOGS Network-Dev instead of Network.")
+    parser.add_argument('--home_azimuth', type=float, default=0.0, help="Home Azimuth. Default=0.0")
+    parser.add_argument('--home_elevation', type=float, default=0.0, help="Home Elevation, Default=0.0")
+    parser.add_argument('--homing_timeout', type=float, default=180.0, help='Overall homing timeout. Default = 180 seconds.')
+    parser.add_argument('--azimuth_step', type=float, default=90.0, help="Move in X degree steps. Default = 90 degrees")
+    parser.add_argument('--movement_threshold', type=float, default=10.0, help="Movement threshold. Default = 10 degrees")
+    parser.add_argument('--rotctld_host', type=str, default="127.0.0.1", help="rotctld hostname. Default=127.0.0.1")
+    parser.add_argument('--rotctld_port', type=int, default=4533, help="rotctld port. Default=4533")
+    args = parser.parse_args()
+
+    _home_az = args.home_azimuth
+    _home_el = args.home_elevation
+
+    # Attempt to get the next observation rise azimuth.
+    if args.station_id != -1:
+        (_rise_az, _time_to_obs) = get_next_rise_azimuth(args.station_id, args.network_dev)
+
+        if _rise_az is not None:
+            if _time_to_obs < args.homing_timeout:
+                logging.critical("Next observation is only %s seconds away! Not enough time to move..." % _time_to_obs)
+                sys.exit(0)
+
+            logging.info("Next observation rises at %.1f degrees, in %.1f minutes." % (_rise_az, _time_to_obs/60.0))
+            _home_az = _rise_az
+
+        else:
+            logging.info("No observations scheduled, using home position.")
+
+
+    # Attempt to connect to ROTCTLD
+    _rot = ROTCTLD(
+        hostname = args.rotctld_host,
+        port = args.rotctld_port,
+        poll_rate = 3.0,
+        movement_threshold = args.movement_threshold
+        )
+
+    _start_time = time.time()
+
+    logging.info("Attempting to move to target position: %.1f, %.1f" % (_home_az, _home_el))
+
+    # Keep trying to move until we hit the timeout.
+    while (time.time() - _start_time) < args.homing_timeout:
+        # First up, get the current rotator position
+        (_az, _el) = _rot.get_azel()
+
+        # Check the position is valid.
+        if _az is None:
+            logging.error("Could not communicate with rotator.")
+            break
+
+        if (abs(_home_az - _az) < args.movement_threshold) and (abs(_home_el - _el) < args.movement_threshold):
+            logging.info("Finished moving to target position.")
+            # One more time, with feeling...
+            _rot.set_azel(_home_az, _home_el, blocking=False)
+            break
+
+        if _az < _home_az:
+            # Move clockwise
+            _new_az = _az + args.azimuth_step
+            # We're close enough that we don't need to move in steps any more, just go straight there.
+            if _new_az > _home_az:
+                _new_az = _home_az
+
+        elif _az > _home_az:
+            # Move anticlockwise.
+            _new_az = _az - args.azimuth_step
+
+            if _new_az < home_az:
+                _new_az = home_az
+
+        # Now modulo the new target azimuth so that we can send it to the rotator.
+        _new_az = _new_az % 360.0
+
+        # Command the rotator!
+        try:
+            _success = _rot.set_azel(_new_az, _home_el, blocking=True)
+        except Exception as e:
+             logging.error("Error - %s" % str(e))
+             break
+
+        # Check the command was successful.
+        if _success:
+            logging.info("Movement successful!")
+        else:
+            logging.error("Failed to move rotator. Breaking")
+            break
+
+
+    # Finished!
+    _rot.close()
+    logging.info("Elapsed time: %d seconds" % (time.time() - _start_time))
+
+
+
+
+
+
+
+
+
+
+
